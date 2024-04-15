f3acc7a76f376f9f567cc288b579134b70a42b1c
Jānis Basovs <janis_basis@hotmail.com>
2024-04-15 11:33:29 +0300

    Slightly updated test

--- src/pages/todays-birthdays/components/birthdays-error-modal/birthdays-error-modal.test.tsx
+++ src/pages/todays-birthdays/components/birthdays-error-modal/birthdays-error-modal.test.tsx
@@ -6,23 +6,20 @@ import { BirthdaysErrorModal } from './birthdays-error-modal.component'
 
 describe('BirthdaysErrorModal', () => {
   it("Does 'Todays Birthdays Error Modal' component render", async () => {
+    const { result } = renderHook(() => useGetBirthdaysStore())
+
+    console.log('result.current.isError 111 -> ', result.current.isError)
+
+    act(() => {
+      result.current.setIsError(true)
+    })
+
+    console.log('result.current.isError 222 -> ', result.current.isError)
+
+    expect(result.current.isError).toEqual(true)
+
     render(<BirthdaysErrorModal />)
-
-    const {
-      result: {
-        current: { isError, setIsError },
-      },
-    } = renderHook(() => useGetBirthdaysStore())
-    console.log('isErrorisError -> ', isError)
-
-    act(() => setIsError(true))
-    console.log('isErrorisError222 -> ', isError)
-
-    const title = await screen.findByTestId('fetch-birthdays-error-modal-title')
-
-    expect(title).toBeInTheDocument()
-    expect(title).toHaveTextContent('Error')
-
-    screen.debug()
+    const errorModalTitle = await screen.findByTestId('fetch-birthdays-error-modal-title')
+    expect(errorModalTitle).toHaveTextContent(/error/i)
   })
 })
